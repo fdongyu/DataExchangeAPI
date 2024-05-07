5bed4afb6f15fb8bae1fd9a94758b7d4c1006d23
Ayush Manojkumar Lodha <63442869+ayushlodha7@users.noreply.github.com>
2024-05-07 12:04:42 -0400

    Update e3sm_test.f90

--- e3sm_test.f90
+++ e3sm_test.f90
@@ -5,7 +5,7 @@ program e3sm_test
     integer(c_int), dimension(5) :: id
 
     ! Set the server URL at runtime
-    call set_server_url("http://10.249.0.64:8080")
+    call set_server_url("http://10.252.1.76:8080")
 
 
     ! User sets values directly
@@ -13,10 +13,10 @@ program e3sm_test
     sd%destination_model_ID = 2005
     sd%initiator_id = 35
     sd%inviter_id = 36
-    sd%input_variables_ID = [1, 2, 3]
-    sd%input_variables_size = [20, 20, 20]
-    sd%output_variables_ID = [4, 5, 6]
-    sd%output_variables_size = [20, 20, 20]
+    sd%input_variables_ID = [1]
+    sd%input_variables_size = [300000000]
+    sd%output_variables_ID = [4]
+    sd%output_variables_size = [300000000]
 
     ! write the session_ID for whole program
     id = [2001, 2005, 35, 36, 1]
@@ -31,7 +31,7 @@ program e3sm_test
     call sleep(10)
 
     ! Join the session (if needed)
-    ! call join_session()
+    ! call join_current_session()
     ! print *, "------ Sleeping for 10 seconds ------"
     ! call sleep(10)
 
@@ -46,6 +46,6 @@ program e3sm_test
     call sleep(10)
 
     ! End the session
-    call end_session()
+    call end_session_now()
 
 end program e3sm_test
